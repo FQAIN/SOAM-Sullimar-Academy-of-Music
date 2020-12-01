16df83468e96a57ce2aed2a6b319c49c807dc538
JoeM <joe.moloney496@gmail.com>
2020-12-01 13:29:30 +0000

    Added JM_StoredProcedures - 50% Complete
    listOfRequirements - highlighted completed
    Created:
    JM_Debug / DBGeneration.sql, insertData.sql, JM_StoredProcedures.sql

---
+++ Database Definitions/JM_Debug/JM_StoredProcedures.sql
@@ -0,0 +1,68 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `viewEventDetails`()
+    NO SQL
+BEGIN
+	SELECT * FROM events;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `viewAllStudentDetails`()
+    NO SQL
+BEGIN
+	SELECT * FROM student;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addEvent`(IN `p_name` VARCHAR(45), IN `p_description` VARCHAR(45), IN `p_dateOfEvent` DATE, IN `p_price` DECIMAL(6,2), IN `p_location` VARCHAR(45), IN `p_image` VARCHAR(45))
+    NO SQL
+BEGIN
+	INSERT INTO events
+    (
+    	name,
+        description,
+        dateOfEvent,
+        price,
+        location,
+        image
+    )
+    VALUES
+    (
+    	p_name,
+        p_description,
+        p_dateOfEvent,
+        p_price,
+        p_location,
+        p_image
+    );
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateStudent`(IN `p_memberid` INT, IN `p_username` VARCHAR(45), IN `p_password` VARCHAR(45), IN `p_email` VARCHAR(45), IN `p_dob` DATE, IN `p_phone` INT, IN `p_dateEnrollment` DATE, IN `p_fName` VARCHAR(45), IN `p_lName` VARCHAR(45), IN `p_address` VARCHAR(45), IN `p_image` VARCHAR(45))
+    NO SQL
+BEGIN
+	UPDATE student
+    SET
+    username = p_username,
+    password = p_password,
+    email = p_email,
+    dob = p_dob,
+    phone = p_phone,
+    dateEnrollment = p_dateEnrollment,
+    fName = p_fName,
+    lName = p_lName,
+    address = p_address,
+    image = p_image
+    WHERE p_memberid = memberID;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `viewCourseDetails`()
+    NO SQL
+BEGIN
+	SELECT * FROM courses;
+END$$
+DELIMITER ;
